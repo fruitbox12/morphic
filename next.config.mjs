a007346a58fee28a88380daafd78588ae53bf776
fruitbox12 <dylanwong007@gmail.com>
2024-08-29 14:22:55 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,7 @@
  */
 const nextConfig = {
   reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-    eslint: {
-    ignoreDuringBuilds: true,
-  },
+ 
 };
 
 export default nextConfig;
