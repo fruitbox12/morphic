8559c1468a2b416224cd2a4ce35aa9e95b0cc8e7
fruitbox12 <dylanwong007@gmail.com>
2024-08-29 14:11:52 -0700

    Update PlaidAuth.tsx

--- components/PlaidAuth.tsx
+++ components/PlaidAuth.tsx
@@ -1,3 +1,5 @@
+"use client"; // Add this at the top of the file
+
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { usePlaidLink } from 'react-plaid-link';
@@ -33,5 +35,10 @@ export default function PlaidAuth({ linkToken }: { linkToken: string }) {
     }
   }, [ready, open]);
 
-  return null;
+  return (
+    <div>
+      <h1>Connecting to Plaid...</h1>
+      <p>Please wait while we connect your account.</p>
+    </div>
+  );
 }
