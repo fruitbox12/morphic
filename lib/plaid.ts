468b43e57a5d6ac867822ac7b14a04999baabbcc
fruitbox12 <dylanwong007@gmail.com>
2024-08-29 12:21:20 -0700

    Create plaid.ts

---
+++ lib/plaid.ts
@@ -0,0 +1,24 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const plaidClient = new PlaidApi(
+  new Configuration({
+    basePath: PlaidEnvironments[process.env.PLAID_ENV],
+    baseOptions: {
+      headers: {
+        'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+        'PLAID-SECRET': process.env.PLAID_SECRET,
+        'Plaid-Version': '2020-09-14',
+      },
+    },
+  })
+);
+
+const sessionOptions = {
+  cookieName: 'myapp_cookiename',
+  password: 'complex_password_at_least_32_characters_long',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+};
+
+export { plaidClient, sessionOptions };
