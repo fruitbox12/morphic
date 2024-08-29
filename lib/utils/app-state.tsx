ab8b382d87d5f2450e879f7130c10b77a7e6246d
fruitbox12 <dylanwong007@gmail.com>
2024-08-29 12:29:31 -0700

    Update app-state.tsx

--- lib/utils/app-state.tsx
+++ lib/utils/app-state.tsx
@@ -2,19 +2,21 @@
 
 import { createContext, useState, ReactNode, useContext } from 'react'
 
-const AppStateContext = createContext<
-  | {
-      isGenerating: boolean
-      setIsGenerating: (value: boolean) => void
-    }
-  | undefined
->(undefined)
+interface AppState {
+  isGenerating: boolean;
+  setIsGenerating: (value: boolean) => void;
+  plaidData: any;  // Add this to store Plaid data
+  setPlaidData: (data: any) => void;  // Function to update Plaid data
+}
+
+const AppStateContext = createContext<AppState | undefined>(undefined)
 
 export const AppStateProvider = ({ children }: { children: ReactNode }) => {
   const [isGenerating, setIsGenerating] = useState(false)
+  const [plaidData, setPlaidData] = useState(null)  // Initialize Plaid data as null
 
   return (
-    <AppStateContext.Provider value={{ isGenerating, setIsGenerating }}>
+    <AppStateContext.Provider value={{ isGenerating, setIsGenerating, plaidData, setPlaidData }}>
       {children}
     </AppStateContext.Provider>
   )
