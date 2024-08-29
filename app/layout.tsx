caf71401ad861a0b2d3d19ab93f200a6f6ef4664
fruitbox12 <dylanwong007@gmail.com>
2024-08-29 13:31:59 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata, Viewport } from 'next'
-import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '@/components/theme-provider'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-import { Sidebar } from '@/components/sidebar'
-import { Toaster } from '@/components/ui/sonner'
-import { AppStateProvider } from '@/lib/utils/app-state'
+import type { Metadata, Viewport } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { Sidebar } from '@/components/sidebar';
+import { Toaster } from '@/components/ui/sonner';
+import { AppStateProvider } from '@/lib/utils/app-state';
 
 const fontSans = FontSans({
   subsets: ['latin'],
-  variable: '--font-sans'
-})
+  variable: '--font-sans',
+});
 
-const title = 'Morphic'
-const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+const title = 'Morphic';
+const description = 'A fully open-source AI-powered answer engine with a generative UI.';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://morphic.sh'),
@@ -24,27 +23,27 @@ export const metadata: Metadata = {
   description,
   openGraph: {
     title,
-    description
+    description,
   },
   twitter: {
     title,
     description,
     card: 'summary_large_image',
-    creator: '@miiura'
-  }
-}
+    creator: '@miiura',
+  },
+};
 
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   minimumScale: 1,
-  maximumScale: 1
-}
+  maximumScale: 1,
+};
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -65,5 +64,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
